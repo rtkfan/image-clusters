7dcd9a85e94018e7c06af22222886e6a4dc9e40d
Matt Lam <rtkfan@gmail.com>
2021-12-25 23:09:10 -0800

    add logging

--- clustering.py
+++ clustering.py
@@ -1,9 +1,17 @@
 from icecream import ic
 import os
+import logging
 
 import numpy as np
+
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.INFO)
 
 DATADIR = './images'
 
 with os.scandir(DATADIR) as files:
-    filenames = [ifile.name for ifile in files if ifile.name.endswith(('.png','.jpg','.gif'))]
+    filenames = [ifile.name for ifile in files
+                   if ifile.name.endswith(('.png','.jpg','.gif'))]
+
+logging.info(f'Found {len(filenames)} images in data directory')
